1e9d4088154bd09800fe12c7ed8654677c3b0fe7
artblane <8blane@gmail.com>
2024-12-17 19:35:40 -0800

    random placement
    worked on random placement of asteroids

--- Assets/scripts/HealthPack.cs
+++ Assets/scripts/HealthPack.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class asteroid : MonoBehaviour{
+public class healthPack : MonoBehaviour{
 public GameObject prefab;
+
 
     void PlaceRandomly(GameObject obj)
     {
@@ -14,19 +15,13 @@ public GameObject prefab;
         float x = Random.Range(-400f,400f);
         float z = Random.Range(-400f,400f);
         Vector3 randomPosition = new Vector3(x, 75, z);
-        
-        //Random Scale
-        float xScale = Random.Range(250.0f,850.0f);
-        float zScale = Random.Range(250.0f,850.0f);
-        float yScale = Random.Range(250.0f,850.0f);
-        Vector3 randomScale = new Vector3(xScale,zScale,yScale);
-        
 
         // Set the new position and scale of the object
         obj.transform.position = randomPosition;
-        obj.transform.localScale = randomScale;
+    
     }
-// for loop to instantiate several random orienations of the asteroid
+
+    // for loop to instantiate several random orienations of the asteroid
     void Start () { 
     for(int i = 0; i < 200; ++i)
 {
@@ -38,23 +33,8 @@ public GameObject prefab;
         //Return the new Quaternion
         
     
-    PlaceRandomly(Instantiate(prefab, new Vector3(0,75, 0), newQuaternion));
+    PlaceRandomly(Instantiate(prefab, new Vector3(0,10,0), newQuaternion));
 
-    
+    }
+    }
 }
-    
-    }
-   
- 
-   
-    void Update ()
-    {
-    
-    } 
-
-    }
-
-   
-
-    
-
