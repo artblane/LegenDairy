db69ff43ed1ae4dc5a3a483daca9c31a7d49d5df
Lindsey Short <shortlindsk@gmail.com>
2024-12-19 04:05:30 -0800

    Revert "Win/Lose scenes connected"
    This reverts commit 16c55a59b1bf639b1e7200176489ee2b55a0c17b.

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,67 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float health, MaxHealth;
+    public AudioSource AudioPlayerHealth;
+
+    public AudioSource AudioPlayerCrash;
+
+    public AudioSource AudioPlayerPlanetCrash;
+
+    public AudioSource AudioPlayerFinishLine;
+    
+
     public float playerSpeed = 200f;
     public float rotationSpeed = 20f;
-
-    // Healthbar UI
+    //healthbar UI 
     [SerializeField]
     private HealthBarUI healthBar;
 
-    // Set health at start
-    void Start()
-    {
+//set health at start
+    void Start () {
         healthBar.SetMaxHealth(MaxHealth);
-        Debug.Log("health = " + health);
+    Debug.Log("health = " + health);
     }
-
-    void FixedUpdate()
+   
+   
+    void FixedUpdate ()
     {
-        // Player movements
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
 
-        GetComponent<Rigidbody>().AddForce(-transform.right * verticalInput * playerSpeed);
-        transform.Rotate(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
+    
+//player movements
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");   
+    
+    GetComponent<Rigidbody>().AddForce(-transform.right * verticalInput * playerSpeed);
+    transform.Rotate(0,horizontalInput * rotationSpeed * Time.deltaTime,0);
+
     }
+    
+    
+public void SetHealth(float healthChange){
+    health += healthChange;
+    health = Mathf.Clamp(health, 0, MaxHealth);
+    healthBar.SetHealth(health);
+}
+//asteroid, healthpack, and planet triggers
+    public void OnTriggerEnter(Collider other){
+        
+        if(other.gameObject.CompareTag("healthPack")){
+        SetHealth(10f);
+        Debug.Log("HEALTH PACK GET, health is now "+ health);
+        AudioPlayerHealth.Play();
+        Destroy(other.gameObject);
+        }
+        else if(other.gameObject.CompareTag("asteroid")){
+        SetHealth(-5f);
+        Debug.Log("OUCH! health is now= "+ health);
+        AudioPlayerCrash.Play();
+        }
+        else if(other.gameObject.CompareTag("planet")){
+        SetHealth(-15f);
+        Debug.Log("BIG OUCH! health is now= "+ health);
+        AudioPlayerPlanetCrash.Play();
+        }
+         else if(other.gameObject.CompareTag("finishLine")){
+        AudioPlayerFinishLine.Play();
+        }
 
-    public void SetHealth(float healthChange)
-    {
-        health += healthChange;
-        health = Mathf.Clamp(health, 0, MaxHealth);
-        healthBar.SetHealth(health);
-
-        // Check for game over when health reaches zero
-        if (health <= 0)
+        // check for health game over 
+if (health <= 0)
         {
             Debug.Log("Game Over");
-            SceneManager.LoadScene("Lose"); // Transition to Lose scene
+        }
         }
     }
 
-    // Asteroid, health pack, and planet triggers
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("healthPack"))
-        {
-            SetHealth(10f);
-            Debug.Log("HEALTH PACK GET, health is now " + health);
-            Destroy(other.gameObject);
-        }
-        else if (other.gameObject.CompareTag("asteroid"))
-        {
-            SetHealth(-5f);
-            Debug.Log("OUCH! health is now= " + health);
-        }
-        else if (other.gameObject.CompareTag("planet"))
-        {
-            SetHealth(-15f);
-            Debug.Log("BIG OUCH! health is now= " + health);
-        }
-    }
-}
+
+
+
